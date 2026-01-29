16f5759745d42bd3cc77f52906633badff41c05d
JOSEPH ANDREW YANSON ONG <2302124@cc.usls.edu>
2026-01-29 14:31:39 +0800

    lab activity 3: database and eloquent ORM

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-        <title>
-            @yield('title', 'My Portfolio')
-    </title>
+
+    <title>@yield('title', 'My Portfolio')</title>
+
+    <link 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+    >
 </head>
 <body>
-    
